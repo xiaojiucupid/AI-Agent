9632309e9bc9b2f59563cfac632299e0a87d46ef
fuzhengwei <184172133@qq.com>
2025-05-30 22:08:26 +0800

    feat: dev-ops-v2

--- ai-agent-station-app/build.sh
+++ ai-agent-station-app/build.sh
@@ -2,4 +2,4 @@
 docker build -t fuzhengwei/ai-agent-station-app:1.0.1 -f ./Dockerfile .
 
 # 兼容 amd、arm 构建镜像
-# docker buildx build --load --platform liunx/amd64,linux/arm64 -t xiaofuge/xfg-frame-archetype-app:1.0 -f ./Dockerfile . --push
+# docker buildx build --load --platform linux/amd64,linux/arm64 -t fuzhengwei/ai-agent-station-app:1.0.2 -f ./Dockerfile .
