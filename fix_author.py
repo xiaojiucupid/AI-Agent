e8c662da5eda9fc771234f498b88570db12986f2
@gcw_iSltDj8Q <xiaojiucupid@gmail.com>
2026-04-22 16:06:33 +0800

    本地提交

---
+++ fix_author.py
@@ -0,0 +1,5 @@
+def callback(commit, metadata):
+    commit.author_name = b"xiaojiucupid"
+    commit.author_email = b"xiaojiucupid@gmail.com"
+    commit.committer_name = b"xiaojiucupid"
+    commit.committer_email = b"xiaojiucupid@gmail.com"
